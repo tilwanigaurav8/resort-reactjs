6031609048fdc62021c84c13048c41569596571a
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-05-21 17:17:05 +0530

    working-with-container

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,5 +1,36 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
+import RoomFilter from './RoomFilter';
+import RoomList from './RoomList';
+
+function RoomsContainer({context}){
+    const {loading,sortedRooms,rooms}=context;
+    if(loading){
+        return <Loading/>;
+    }
+    return (
+        <div>
+            <p> Hello froms Rooms Container </p>
+            <RoomFilter rooms={rooms}/>
+            <RoomList rooms={sortedRooms} />
+        </div>
+    )
+}
+
+export default withRoomConsumer(RoomsContainer);
+
+
+
+
+
+
+
+
+/*
+import { render } from "@testing-library/react";
+import React from "react";
 import { RoomConsumer } from "../context";
 import Loading from "./Loading";
 import RoomFilter from './RoomFilter';
@@ -10,15 +41,23 @@ const RoomsContainer=()=>{
             <RoomConsumer>
                 {
                     (value)=>{
-                        return 
+                        const {loading,sortedRooms,rooms}=value;
+                        if(loading){
+                            return <Loading/>;
+                        }
+                        return (
+                            <div>
+                                <p> Hello froms Rooms Container </p>
+                                <RoomFilter rooms={rooms}/>
+                                <RoomList rooms={sortedRooms} />
+                            </div>
+                        )
                     }
                 }
             </RoomConsumer>
-            <p> Hello froms Rooms Container </p>
-            <RoomFilter />
-            <RoomList />
         </>
         
     )
 }
 export default RoomsContainer;
+*/
